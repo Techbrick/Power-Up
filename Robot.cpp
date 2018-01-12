b7f62c8de4c688ba5e631f8febad9151159510c6
dracojoseph <dracojoseph@gmail.com>
2018-01-11 20:51:45 -0500

    Update Robot.cpp

--- Robot.cpp
+++ Robot.cpp
@@ -6,18 +6,18 @@ Robot::Robot() :
 		rearLeftMotor(Constants::rearLeftDriveChannel),
 		frontRightMotor(Constants::frontRightDriveChannel),
 		rearRightMotor(Constants::rearRightDriveChannel),
-		robotDrive(frontLeftMotor, rearLeftMotor, frontRightMotor, rearRightMotor),
+		robotDrive(frontLeftMotor, frontRightMotor),
 		driveStick(Constants::driveStickChannel),
 		operatorStick(Constants::operatorStickChannel),
 		gyro(SPI::Port::kMXP, 200),
 		compressor(),
 		pdp()
 {
+	robotDrive.SetExpiration(0.1);
 	gyro.ZeroYaw();
-	robotDrive.SetInvertedMotor(RobotDrive::kFrontLeftMotor, false);
-	robotDrive.SetInvertedMotor(RobotDrive::kRearLeftMotor, false);
-	robotDrive.SetInvertedMotor(RobotDrive::kFrontRightMotor, true);
-	robotDrive.SetInvertedMotor(RobotDrive::kRearRightMotor, true);
+	rearLeftMotor.Set(3.0,Constants::frontLeftDriveChannel);
+	rearRightMotor.Set(3.0,Constants::frontRightDriveChannel);
+	//robotDrive.SetInvertedMotor(RobotDrive::kFrontLeftMotor, false);
 	rearLeftMotor.ClearStickyFaults(); //talons browned out and got a low battery fault so they were compensating - this should even out the power and improve drive (especially strafing)
 	rearRightMotor.ClearStickyFaults(); //talons browned out and got a low battery fault so they were compensating - this should even out the power and improve drive (especially strafing)
 	frontLeftMotor.ClearStickyFaults(); //talons browned out and got a low battery fault so they were compensating - this should even out the power and improve drive (especially strafing)
@@ -30,6 +30,22 @@ void Robot::RobotInit()
 
 void Robot::OperatorControl()
 {
+
+	robotDrive.SetSafetyEnabled(false);
+	gyro.Reset();
+	gyro.ResetDisplacement();
+
+	while(IsOperatorControl() && IsEnabled())
+	{
+
+		robotDrive.ArcadeDrive(driveStick.GetRawAxis(2),-1*driveStick.GetRawAxis(1));
+
+		frc::Wait(0.005);
+
+	}
+
+	robotDrive.SetSafetyEnabled(true);
+
 }
 
 void Robot::Autonomous()
