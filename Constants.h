c6a7e9ba82e98e1105e3abf854712326d3e18d47
dracojoseph <dracojoseph@gmail.com>
2018-01-11 20:52:54 -0500

    Create Constants.h

---
+++ Constants.h
@@ -0,0 +1,16 @@
+#ifndef _CONSTANTS_H
+#define _CONSTANTS_H
+
+namespace Constants
+{
+
+	static constexpr int frontLeftDriveChannel = 12;
+	static constexpr int rearLeftDriveChannel = 13;
+	static constexpr int frontRightDriveChannel = 14;
+	static constexpr int rearRightDriveChannel = 15;
+	static constexpr int driveStickChannel = 0;
+	static constexpr int operatorStickChannel = 99;
+
+}
+
+#endif
